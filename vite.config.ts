176f48f7479669319b8a2154158a555ebb616dcd
Sandeep Reddy Yeruva <79009504+Sandeep3899@users.noreply.github.com>
2025-03-11 15:01:58 -0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "https://sandeep3899.github.io/Portfolio/",
+  base: "/Portfolio/",
   plugins: [react()],
 });
