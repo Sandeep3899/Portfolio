1348a108e01cc0dcee5987e24461167df8d7acc7
Sandeep Reddy Yeruva <79009504+Sandeep3899@users.noreply.github.com>
2025-03-11 14:50:00 -0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Portfolio/",  // Use your repository name here
+  base: "https://sandeep3899.github.io/Portfolio/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
